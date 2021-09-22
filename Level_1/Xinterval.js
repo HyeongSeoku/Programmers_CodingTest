3203829683742d0b709303b5eebe10aa0573acd6
gudtjr3437 <gudtjr3437@naver.com>
2021-09-22 16:57:56 +0900

    X만큼 간격이 있는 N개의 숫자

---
+++ Level_1/Xinterval.js
@@ -0,0 +1,13 @@
+/*
+문제 이름 : x 만큼 간격이 있는 n개의 숫자
+링크 : https://programmers.co.kr/learn/courses/30/lessons/12954 
+ */
+
+function solution(x, n) {
+  var answer = [];
+  for (let i = 1; i <= n; i++) {
+    answer.push(x * i);
+  }
+
+  return answer;
+}
