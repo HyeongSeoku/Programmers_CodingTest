497d332dd8552087a4771bc04b623608040f4287
gudtjr3437 <gudtjr3437@naver.com>
2021-09-27 10:58:23 +0900

    문자열을 정수로 바꾸기

---
+++ Level_1/NumTranslater.js
@@ -0,0 +1,14 @@
+/*
+문제 이름 : 문자열을 정수로 바꾸기
+링크 : https://programmers.co.kr/learn/courses/30/lessons/12925?language=javascript
+ */
+
+function tanslater(s) {
+  return +s;
+
+  //다른 풀이_1
+  //return Number(s);
+
+  //다른 풀이_2
+  //return s/1
+}
