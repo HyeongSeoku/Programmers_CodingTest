9f779c9bacf2d38455ac11b5f6c220cfaff91da7
gudtjr3437 <gudtjr3437@naver.com>
2021-09-29 23:44:25 +0900

    문자열 내림차순으로 배치하기

---
+++ Level_1/StringDescending.js
@@ -0,0 +1,10 @@
+/*
+문제 이름 : 문자열 내림차순으로 배치하기
+링크 : https://programmers.co.kr/learn/courses/30/lessons/12917?language=javascript
+ */
+
+function stringDescSort(s) {
+  var answer = "";
+
+  return answer;
+}
