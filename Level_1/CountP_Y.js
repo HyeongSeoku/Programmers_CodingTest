03a72528be2c9834a202a4e2b944830db66917b0
gudtjr3437 <gudtjr3437@naver.com>
2021-10-01 21:27:31 +0900

    문자열 내 p와 y의 개수

---
+++ Level_1/CountP_Y.js
@@ -0,0 +1,26 @@
+/*
+문제 이름 : 문자열 내 p와 y의 개수
+링크 : https://programmers.co.kr/learn/courses/30/lessons/12916?language=javascript
+ */
+
+function CountP_Y(s) {
+  var answer = true;
+  var pCount = 0,
+    yCount = 0;
+  var sArr = [];
+  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+  s = s.toLowerCase();
+  sArr = s.split("");
+  for (var i = 0; i < sArr.length; i++) {
+    if (sArr[i] === "p") {
+      pCount++;
+    } else if (sArr[i] === "y") {
+      yCount++;
+      console.log(sArr[i]);
+    }
+  }
+  if (pCount !== yCount) {
+    answer = false;
+  }
+  return answer;
+}
