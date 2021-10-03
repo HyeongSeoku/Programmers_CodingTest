93f38fb32454a4fabe3c3680026ec255a7878bd9
gudtjr3437 <gudtjr3437@naver.com>
2021-10-03 13:10:08 +0900

    직사각형 별찍기

---
+++ python/Level_1_py/SquareStar.py
@@ -0,0 +1,9 @@
+'''
+문제 이름 : 두 정수 사이의 합
+링크 : https://programmers.co.kr/learn/courses/30/lessons/12912?language=javascript
+'''
+
+a, b = map(int, input().strip().split(' '))
+str = '*'*a
+for i in range(0,b):
+        print(str)
